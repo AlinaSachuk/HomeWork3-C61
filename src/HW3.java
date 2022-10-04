ff797be1cf2cba725fd8e4222712f1cbe153b40a
AlinaSachuk <alinko-91@yandex.ru>
2022-10-04 22:45:15 +0300

    HW3

--- src/HW3.java
+++ src/HW3.java
@@ -1,2 +1,54 @@
+import java.util.Scanner;
+
 public class HW3 {
+    public static void main(String[] args) {
+        System.out.println("Домашняя работа №3");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ввведите размер массива в виде числа");
+        int someValue = scanner.nextInt();
+        double array[] = new double[someValue];
+        System.out.println("массив заполнен методом Math.random");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random();
+            System.out.println(array[i]);
+        }
+        System.out.println("Задание 1 - элементы массива в прямом порядке");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println("Задание 1 - элементы массива в обратном порядке");
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.println(array[i]);
+        }
+        System.out.println("Задание 2 - максимальный элемент массива");
+        double max = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+            System.out.println(max);
+        System.out.println("Задание 2 - минимальный элемент массива");
+        double min = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                System.out.println(min);
+            }
+        }
+        System.out.println("Задание 3");
+
+        System.out.println("Задание 4");
+        for (int i = 0; i < array.length; i++) {
+            if (array [i] == 0) {
+                System.out.println(array[i]);
+            } else {
+                System.out.println("нулевых элементов в массиве нет");
+                break;
+            }
+        }
+        System.out.println("Задание 5");
+        System.out.println("Задание 6");
+    }
 }
+
