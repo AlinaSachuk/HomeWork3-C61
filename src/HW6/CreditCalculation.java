2d693ba00b18cc524779966cb629de4ec6fdabd8
AlinaSachuk <alinko-91@yandex.ru>
2022-10-16 10:24:57 +0300

    HW6 right complete (deleted unnecessary classes)

--- src/HW6/CreditCalculation.java
+++ src/HW6/CreditCalculation.java
@@ -2,17 +2,23 @@ package HW6;
 
 public class CreditCalculation {
     public static void main(String args[]) {
-        System.out.println("Информация по карте №1");
-        Card1 card1 = new Card1();
-        card1.CreditCardInfo();
-        System.out.println("сумма на счету карты №1 после зачисления - " + card1.accrualSum(card1.sumOnCard, 364.0));
-        System.out.println("Информация по карте №2");
-        Card2 card2 = new Card2();
-        card2.CreditCardInfo();
-        System.out.println("сумма на счету карты №2 после зачисления - " + card2.accrualSum(card2.sumOnCard, 1943));
-        System.out.println("Информация по карте №3");
-        Card3 card3 = new Card3();
-        card3.CreditCardInfo();
-        System.out.println("сумма на счету карты №3 после снятия - " + card3.getMoney(card3.sumOnCard, 89.2));
+        System.out.print("Информация по карте №1: ");
+        CreditCard card1 = new CreditCard(12341234, 187.32);
+        card1.getCreditCardInfo();
+        System.out.print("Информация по карте №1 после зачисления: ");
+        card1.accrualSum(13);
+        card1.getCreditCardInfo();
+        System.out.print("Информация по карте №2: ");
+        CreditCard card2 = new CreditCard(56435643, 10.34);
+        card2.getCreditCardInfo();
+        card2.accrualSum(567);
+        System.out.print("Информация по карте №2 после зачисления: ");
+        card2.getCreditCardInfo();
+        System.out.print("Информация по карте №3: ");
+        CreditCard card3 = new CreditCard(34523452, 984.2);
+        card3.getCreditCardInfo();
+        card3.getMoney(456);
+        System.out.print("Информация по карте №2 после снятия: ");
+        card3.getCreditCardInfo();
     }
 }
--- src/HW6/CreditCard.java
+++ src/HW6/CreditCard.java
@@ -4,18 +4,20 @@ public class CreditCard extends CreditCalculation {
     int accountNumber;
     double sumOnCard;
 
-    public CreditCard() {
+    public CreditCard(int accountNumber, double sumOnCard) {
+        this.accountNumber = accountNumber;
+        this.sumOnCard = sumOnCard;
     }
 
-    public double accrualSum(double sumOnCard, double x) {
-        return sumOnCard + x;
+    public double accrualSum(double sumToAdd) {
+        return sumOnCard += sumToAdd;
     }
 
-    public double getMoney(double sumOnCard, double y) {
-        return sumOnCard - y;
+    public double getMoney(double sumToGet) {
+        return sumOnCard -= sumToGet;
     }
 
-    public void CreditCardInfo() {
+    public void getCreditCardInfo() {
         System.out.println("Номер счета: " + accountNumber + ", " + "Остаток на счету: " + sumOnCard);
     }
 }
