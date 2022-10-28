5e3b79cf1a5724ef07f5e01ea26bb0e726534278
AlinaSachuk <alinko-91@yandex.ru>
2022-10-28 20:44:17 +0300

    HW10 forth task complete

--- src/HW10/Task4.java
+++ src/HW10/Task4.java
@@ -1,15 +1,30 @@
 package HW10;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите строку 1.");
-        String line1 = scanner.nextLine();
-        System.out.println("Введите строку 2.");
-        String line2 = scanner.nextLine();
-        System.out.println("Введите строку 3.");
-        String line3 = scanner.nextLine();
+        System.out.println("Введите необходимое количество строк в виде числа.");
+        int numberOfWords = scanner.nextInt();
+        for (int n = 0; n < numberOfWords; n++) {
+            System.out.println("Введите строку:");
+            String line = scanner.next();
+            char[] array = line.toCharArray();
+            int counter = 0;
+            for (int i = 0; i < line.length(); i++) {
+                for (int j = i + 1; j < line.length(); j++) {
+                    if (array[i] != array[j]) {
+                        counter++;
+                    }
+                }
+            }
+            if (counter > 0) {
+                System.out.print("Слово, состоящее только из различных символов: ");
+                System.out.println(line);
+                break;
+            }
+        }
     }
 }
