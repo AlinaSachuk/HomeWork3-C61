3d0be5cff60f07a851c55b3566f9deb5fac0a16f
AlinaSachuk <alinko-91@yandex.ru>
2022-10-28 16:36:08 +0300

    HW10 third task complete

--- src/HW10/Task3.java
+++ src/HW10/Task3.java
@@ -11,6 +11,30 @@ public class Task3 {
         String line2 = scanner.nextLine();
         System.out.println("Введите строку 3.");
         String line3 = scanner.nextLine();
-
+        int l1 = line1.length();
+        int l2 = line2.length();
+        int l3 = line3.length();
+        System.out.print("Строки, длинна которых меньше средней: ");
+        if (l1 > l2 & l1 > l3 & l2 > l3) {
+            System.out.println(line3 + ": длинна этой строки - " + l3);
+        } else if (l1 > l2 & l1 > l3 & l3 > l2) {
+            System.out.println(line2 + ": длинна этой строки - " + l2);
+        } else if (l1 > l2 & l1 > l3 & l3 == l2) {
+            System.out.println(line2 + ": длинна этой строки - " + l2 + ", " + line3 + ": длинна этой строки - " + l3);
+        } else if (l2 > l1 & l2 > l3 & l1 > l3) {
+            System.out.println(line3 + ": длинна этой строки - " + l3);
+        } else if (l2 > l1 & l2 > l3 & l3 > l1) {
+            System.out.println(line1 + ": длинна этой строки - " + l1);
+        } else if (l2 > l1 & l2 > l3 & l3 == l1) {
+            System.out.println(line1 + ": длинна этой строки - " + l1 + ", " + line3 + ": длинна этой строки - " + l3);
+        } else if (l3 > l1 & l3 > l2 & l1 > l2){
+            System.out.println(line2 + ": длинна этой строки- " + l2);
+        } else if (l3 > l1 & l3 > l2 & l2 > l1) {
+            System.out.println(line1 + ": длинна этой строки - " + l1);
+        } else if (l3 > l1 & l3 > l2 & l1 == l2) {
+            System.out.println(line1 + ": длинна этой строки - " + l1 + ", " + line2 + ": длинна этой строки - " + l2);
+        } else {
+            System.out.println("Вы ввели не строку. Перезапустите программу и повторите ввод.");
+        }
     }
 }
