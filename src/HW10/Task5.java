f6d3516e0c3c18abef259427718cf23d704fec84
AlinaSachuk <alinko-91@yandex.ru>
2022-10-29 10:04:51 +0300

    HW10 task5 rightly completed

--- src/HW10/Task5.java
+++ src/HW10/Task5.java
@@ -5,20 +5,18 @@ import java.util.Scanner;
 public class Task5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите слово №1, состоящее только из цифр.");
-        StringBuilder line1 = new StringBuilder(scanner.nextLine());
-        System.out.println("Введите слово №2, состоящее только из цифр.");
-        StringBuilder line2 = new StringBuilder(scanner.nextLine());
-        System.out.println("Введите слово №3, состоящее только из цифр.");
-        StringBuilder line3 = new StringBuilder(scanner.nextLine());
-        if (line1.reverse().equals(line1)) {
-            System.out.println(line1 + " - это слово-палиндром");
-        } else if (line2.reverse().equals(line2)) {
-            System.out.println(line2 + " - это слово-палиндром");
-        } else if (line3.reverse().equals(line3)) {
-            System.out.println(line3 + " - это слово-палиндром");
-        } else {
-            System.out.println("Повторите попытку");
+        System.out.println("Введите необходимое количество слов в виде цифры.");
+        int numberOfWords = scanner.nextInt();
+        for (int n = 0; n < numberOfWords; n++) {
+            System.out.println("Введите слово, состоящее только из цифр.");
+            StringBuilder line = new StringBuilder(scanner.next());
+            StringBuilder lineToReverse = new StringBuilder(line.reverse());
+            line.reverse();
+            String x = String.valueOf(line);
+            String y = String.valueOf(lineToReverse);
+            if (x.equals(y)) {
+                System.out.println(line + " - это слово-палиндром");
+            }
         }
     }
 }
